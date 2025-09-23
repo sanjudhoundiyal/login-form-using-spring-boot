4551cafd93e63400ad373c79c42b995cac85283e
Sanjay Dhoundiyal <sanjudhoundiyalsanju@gmail.com>
2025-09-23 13:16:05 +0530

    Add files via upload
     this is java bacend project

---
+++ ProjectSmartContactManagerApplication.java
@@ -0,0 +1,13 @@
+package com.Smart.Contact.Manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSmartContactManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSmartContactManagerApplication.class, args);
+	}
+
+}
